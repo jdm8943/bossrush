fc8b8f36937eb32ceed9108cac3988f6718a23e3
sirpighead <jdm8943@rit.edu>
2023-01-20 19:35:45 -0500

    update script name

--- BossRush/Assets/Scripts/AgentProperties.cs
+++ BossRush/Assets/Scripts/AgentProperties.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerProperties : MonoBehaviour
+public class AgentProperties : MonoBehaviour
 {
+
+    [SerializeField]
+    private int health;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 100;
     }
 
     // Update is called once per frame
