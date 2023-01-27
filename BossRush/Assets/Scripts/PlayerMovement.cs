afb536682a9c017aeefb112ef34bd1cb91579c68
John Huggins <92586058+PrvCaboose@users.noreply.github.com>
2023-01-27 16:59:14 -0500

    Added in dodge roll mechanic, needs to be fixed as you dont stop after its over

--- BossRush/Assets/Scripts/PlayerMovement.cs
+++ BossRush/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,11 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     float speed = 1f;
+    bool isDodging = false;
+    bool dodgeFinished = false;
+    float dodgeTimer = 0f;
+    float lastSecond = 0f;
+    Vector2 lastPlayerInput = new Vector2();
 
     Vector3 vehiclePosition = Vector3.zero;
     Vector3 direction = Vector3.zero;
@@ -32,12 +37,48 @@ public class PlayerMovement : MonoBehaviour
         // “Draw” this vehicle at that position
         transform.position = vehiclePosition;
 
+
+        // TODO: FIX DODGE SO YOU STOP AT THE END
+        //  POSSIBLE FIX: MOVING IT TO SEPERATE FUNCTION WITH SEPERATE MOVEMENT CALCULATIONS
+
+        // Dodge timer
+        if (dodgeTimer <= 1f)
+        {
+            dodgeTimer += Time.deltaTime;
+            lastSecond = dodgeTimer;
+        }
+        else
+        {
+            isDodging = false;
+            GetComponent<SpriteRenderer>().color = Color.white;
+            dodgeTimer = 0f;
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        direction = context.ReadValue<Vector2>();
+        if (isDodging)
+        {
+            return;
+        }
+        lastPlayerInput = context.ReadValue<Vector2>();
+        direction = lastPlayerInput;
 
         transform.rotation = Quaternion.LookRotation(Vector3.back, direction);
+    }
+
+    public void OnDodge(InputAction.CallbackContext context)
+    {
+        isDodging = true;
+        GetComponent<SpriteRenderer>().color = Color.red;
+
+    }
+
+    public void EndDodge()
+    {
+        if (dodgeFinished)
+        {
+
+        }
     }
 }
