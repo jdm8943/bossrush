6625cefcdfcdb9a6d4cdeff19c7cb82f78059ec7
John Huggins <92586058+PrvCaboose@users.noreply.github.com>
2023-01-27 20:39:39 -0500

    Fixed dodge

--- BossRush/Assets/Scripts/PlayerMovement.cs
+++ BossRush/Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     float speed = 1f;
     bool isDodging = false;
-    bool dodgeFinished = false;
     float dodgeTimer = 0f;
-    float lastSecond = 0f;
     Vector2 lastPlayerInput = new Vector2();
-
-    Vector3 vehiclePosition = Vector3.zero;
     Vector3 direction = Vector3.zero;
     Vector3 velocity = Vector3.zero;
 
@@ -26,7 +22,48 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        vehiclePosition = transform.position;
+        if (isDodging)
+        {
+            DodgeMovement();
+        }
+        else
+        {
+            RegularMovement();
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        lastPlayerInput = context.ReadValue<Vector2>();
+
+        if (!isDodging)
+        {
+            direction = lastPlayerInput;
+        }
+    }
+
+    public void OnDodge(InputAction.CallbackContext context)
+    {
+        StartDodge();
+    }
+
+    void RegularMovement()
+    {
+        Vector3 vehiclePosition = transform.position;
+
+        // Velocity is direction * speed * deltaTime
+        velocity = direction * speed * Time.deltaTime;
+
+        // Add velocity to position 
+        vehiclePosition += velocity;
+
+        // “Draw” this vehicle at that position
+        transform.position = vehiclePosition;
+    }
+
+    void DodgeMovement()
+    {
+        Vector3 vehiclePosition = transform.position;
 
         // Velocity is direction * speed * deltaTime
         velocity = direction * speed * Time.deltaTime;
@@ -37,48 +74,23 @@ public class PlayerMovement : MonoBehaviour
         // “Draw” this vehicle at that position
         transform.position = vehiclePosition;
 
-
-        // TODO: FIX DODGE SO YOU STOP AT THE END
-        //  POSSIBLE FIX: MOVING IT TO SEPERATE FUNCTION WITH SEPERATE MOVEMENT CALCULATIONS
-
-        // Dodge timer
         if (dodgeTimer <= 1f)
         {
             dodgeTimer += Time.deltaTime;
-            lastSecond = dodgeTimer;
         }
         else
         {
             isDodging = false;
             GetComponent<SpriteRenderer>().color = Color.white;
             dodgeTimer = 0f;
+            direction = lastPlayerInput;
         }
     }
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        if (isDodging)
-        {
-            return;
-        }
-        lastPlayerInput = context.ReadValue<Vector2>();
-        direction = lastPlayerInput;
-
-        transform.rotation = Quaternion.LookRotation(Vector3.back, direction);
-    }
-
-    public void OnDodge(InputAction.CallbackContext context)
+    void StartDodge()
     {
         isDodging = true;
         GetComponent<SpriteRenderer>().color = Color.red;
-
-    }
-
-    public void EndDodge()
-    {
-        if (dodgeFinished)
-        {
-
-        }
+        direction = lastPlayerInput;
     }
 }
